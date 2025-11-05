764e198467ae8de9313110872701b33c3bfef325
Flamez76 <cianos1310@gmail.com>
2025-11-05 10:38:22 +0000

    Two Classes
    Created User and Module Class

---
+++ src/Module.java
@@ -0,0 +1,24 @@
+class Module {
+    String moduleName;
+    char LabGroup;
+
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public char getLabGroup() {
+        return LabGroup;
+    }
+
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public void setLabGroup(char labGroup) {
+        this.LabGroup = labGroup;
+    }
+
+
+}
